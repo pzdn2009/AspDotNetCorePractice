51b609120d4a540ccb16f2c176d0da746c142ab0
Peng zhen <1050244110@qq.com>
2016-08-09 17:36:13 +0800

    # add todo web api

---
+++ src/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+
+        public ITodoRepository TodoItems { get; set; }
+
+        //GET /api/todo
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        //GET /api/todo/{key}
+        [HttpGet("{id}",Name ="GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        //201 create
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+        }
+
+        //204 no cotent
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(item);
+
+            return new NoContentResult();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Update([FromBody] TodoItem item, string id)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            item.Key = todo.Key;
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        //204 no content 
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
